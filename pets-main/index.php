a0301ead3eb5cae93daefc356f34c711147bc30d
Jcluna05 <73185133+Jcluna05@users.noreply.github.com>
2022-11-15 14:03:28 -0500

    Actualizacion Repositorio

---
+++ pets-main/index.php
@@ -0,0 +1,96 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Happy Pets</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="icon" href="images/logo.png" sizes="32x32" />
+    <link rel="icon" href="images/logo.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="images/logo.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+</head>
+<body>
+<div class="contenedor">
+	<header class="cabeceraPrincipal">
+		<section class="logotipo"><img src="images/logotipoPet.png"></section>
+		<nav class="menuPrincipal">
+			<a href="../">Inicio</a>
+			<a href="internas/servicios.php">Servicios</a>
+			<a href="#">Productos</a>
+			<a href="internas/adopcion.php">Adopción</a>
+			<a href="#">Contactos</a>
+		</nav>
+	</header>
+	<section class="sliderPrincipal">
+		<div class="cuadroadop">
+			<h2>Proyecto Adopción de Mascotas</h2>
+			<button>ADOPTANOS</button>
+		</div>
+		<img src="images/fondo.png">
+	</section>
+	<section class="titulo">
+		<h2>En Adopción</h2>
+	</section>
+	<section class="adopcion1">
+		<img src="images/perrito1.png">
+		<section class="cardog">
+			<h3>Bills</h3>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Et eos pariatur odit, exercitationem consequuntur ea cupiditate dolor nulla
+				nobis quasi facilis officiis harum! Dolores eos dignissimos consequatur blanditiis at aliquid.
+			</p>
+			<button>Más información</button>
+		</section>
+	</section>	
+	
+	<section class="adopcion2">
+		<img src="images/doggy.png">
+		<section class="cardog">
+			<h3>Doggy</h3>
+			<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+				totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+				dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+				sed quia consequuntur magni dolores eos qui
+			</p>
+			<button>Más información</button>
+		</section>
+	</section>	
+
+	<section class="doctores">
+		<section class="doctor1">
+			<img src="images/doctor1.png">
+			<h3>Dr. Juan Cordoba</h3>
+		</section>
+		<section class="doctor2">
+			<img src="images/doctor2.png">
+			<h3>Dr. Carlos Rojas</h3>
+		</section>
+		<section class="doctor3">
+			<img src="images/doctor3.png">
+			<h3>Dr. Juan Perez</h3>
+		</section>
+		<section class="doctor4">
+			<img src="images/doctor4.png">
+			<h3>Dr. Cristina Diaz</h3>
+		</section>
+	</section>
+	<section class="sponsor">
+		<img src="images/logotipoPet.png">
+		<img src="images/logoutpl.png">
+	</section>
+	<footer class="piePagina">
+		<section class="derechos">
+			<h6>Derechos Reservados UTPL 2022</h6>
+		</section>
+		<nav class="redesSociales">
+			<a href=""><i class="fa-brands fa-facebook"></i></a>
+			<a href=""><i class="fa-brands fa-instagram"></i></a>
+			<a href=""><i class="fa-brands fa-tiktok"></i></a>
+		</nav>
+	</footer>
+</div>
+</body>
+</html>
